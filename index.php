cfc5eea57664cbb58aac66ad6a442109334c978a
Robbert <rddebruijne1993@gmail.com>
2025-10-10 13:32:07 +0200

    basis met array opzetten

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+// Global array of authors (simple array)
+$authors = ["J.K. Rowling", "Stephen King", "Dan Brown"];
+
+// Global array of books (multi-dimensional array)
+$books = [
+    [
+        "title" => "Harry Potter",
+        "author" => "J.K. Rowling",
+        "isbn" => "978-1234567890",
+        "publisher" => "Bloomsbury",
+        "publication_date" => "1997-06-26",
+        "pages" => 320
+    ]
+    // More books will be added here...
+
+];
+
